ebd0aa8927283ff7cfba8f41038b9c698f7a974e
Murtaza Ali <murtazashopify@gmail.com>
2026-08-13 11:15:55 +0500

    Add app favicon

--- app/root.jsx
+++ app/root.jsx
@@ -1,22 +1,49 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
+
+import favicon from "../public/favicon.png";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="preconnect" href="https://cdn.shopify.com/" />
+
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          href={favicon}
+        />
+
+        <link
+          rel="preconnect"
+          href="https://cdn.shopify.com/"
+        />
+
         <link
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
+
         <Meta />
         <Links />
       </head>
+
       <body>
         <Outlet />
+
         <ScrollRestoration />
+
         <Scripts />
       </body>
     </html>
